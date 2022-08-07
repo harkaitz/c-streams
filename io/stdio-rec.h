ea973aa316f79e2358ff6b450481e6fcaf1a4289
Harkaitz Agirre <harkaitzv@gmail.com>
2022-08-07 07:38:39 +0200

    -

--- io/stdio-rec.h
+++ io/stdio-rec.h
@@ -1,13 +1,17 @@
 #ifndef STDIO_REC_H
 #define STDIO_REC_H
+#ifdef FLYCHECK
+#  define _POSIX_C_SOURCE 202200L
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdarg.h>
 
-static inline
-int rec_fputs(const char *_s, FILE *_fp, bool _escape) {
+static inline int
+rec_fputs(const char *_s, FILE *_fp, bool _escape)
+{
     if (!_escape) return fputs(_s, _fp);
     for (char const *s = _s; *s; s++) {
         if (*s=='\n') {
@@ -18,8 +22,9 @@ int rec_fputs(const char *_s, FILE *_fp, bool _escape) {
     }
 }
 
-static inline
-int rec_fputm(const char *_s, size_t _sz, FILE *_fp, bool _escape) {
+static inline int
+rec_fputm(const char *_s, size_t _sz, FILE *_fp, bool _escape)
+{
     if (!_escape) return fputs(_s, _fp);
     for (size_t i=0; i<_sz; i++) {
         if (_s[i]=='\n') {
@@ -30,8 +35,9 @@ int rec_fputm(const char *_s, size_t _sz, FILE *_fp, bool _escape) {
     }
 }
 
-static inline
-int rec_vfprintf(FILE *_fp, bool _escape, const char *_fmt, va_list _va) {
+static inline int
+rec_vfprintf(FILE *_fp, bool _escape, const char *_fmt, va_list _va)
+{
     char          *b               = NULL;
     size_t         bsz             = 0;
     FILE          *fp              = NULL;
@@ -53,8 +59,9 @@ int rec_vfprintf(FILE *_fp, bool _escape, const char *_fmt, va_list _va) {
     return 0;
 }
 
-static inline
-int rec_fprintf(FILE *_fp, bool _escape, const char *_fmt, ...) {
+static inline __attribute__ ((__format__ (__printf__, 3, 4))) int
+rec_fprintf(FILE *_fp, bool _escape, const char *_fmt, ...)
+{
     va_list va;
     va_start(va, _fmt);
     int r = rec_vfprintf(_fp, _escape, _fmt, va);
